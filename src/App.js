586ed0e65689b38376afd0c76510d2d432a41566
Hao Li <lihaokx@gmail.com>
2021-07-09 01:15:14 -0400

    chatbot

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import Education from './component/Education';
 import Footer from "./component/Footer";
 import Projects from './component/Projects';
 import Header from './component/HeaderComponent';
+import Chatbot from 'react-chatbot-kit';
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
+import config from './config';
+
+
 function App() {
   return (
     <div className="App  ">
         <Header/>
+        <div  	className="fixedContainer"  >
+          <div className="row justify-content-end ">
+            <div className ="col-4 justify-content-end">
+              <Chatbot config={config} actionProvider={ActionProvider} className ="chatbotCSS" messageParser={MessageParser} />
+            </div>
+          </div>
+        </div>
         <Education/>
-        
         <Projects/>
         <Footer/>
+        
      </div>
   );
 }
